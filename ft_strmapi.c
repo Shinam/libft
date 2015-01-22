2e8925f319a368fd136c8e99be0abc4d609d0dc4
averkenn <arnaud.verkenne@gmail.com>
2015-01-22 11:02:04 +0100

    soon end

---
+++ ft_strmapi.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 10:56:31 by averkenn          #+#    #+#             */
+/*   Updated: 2015/01/22 10:58:03 by averkenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int i;
+	char *str;
+
+	i = 0;
+	str = ft_strdup(s);
+	while (str[i])
+	{
+		str[i] = f(i, str[i]);
+		i++;
+	}
+	return (str);
+}
