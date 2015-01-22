8f6c3079215035e289626b2ac1c257db641734ac
averkenn <arnaud.verkenne@gmail.com>
2015-01-22 11:07:53 +0100

    hmm

---
+++ ft_strsub.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 11:03:44 by averkenn          #+#    #+#             */
+/*   Updated: 2015/01/22 11:07:43 by averkenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, usnigned int start, size_t len)
+{
+	char *str;
+	size_t i;
+
+	i = 0;
+	if (s)
+	{
+		str = malloc(sizeof(char) * len + 1);
+		if (!str)
+			return (NULL);
+		while (i++ < len)
+			str[i] = s[start + i];
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
