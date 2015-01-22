43ac05bda4ebf2b02431e3f3200dfdda099fea18
averkenn <arnaud.verkenne@gmail.com>
2015-01-22 11:22:42 +0100

    hehhe

---
+++ ft_strtrim.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 11:15:27 by averkenn          #+#    #+#             */
+/*   Updated: 2015/01/22 11:22:14 by averkenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	str = malloc(ft_strlen(s) + 1);
+	while (ft_isspace(s[i]))
+			i++;
+	while (s[i])
+	{
+		str[j] = s[i];
+		i++;
+		j++;
+	}
+	while (ft_isspace(s[i]))
+	{
+		s[j] = '\0';
+		i--;
+		j--;
+	}
+	return (str);
+}
