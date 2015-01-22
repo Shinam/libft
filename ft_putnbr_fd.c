2ded05d9f9a03d614fa01f893c10e00008b651f0
averkenn <arnaud.verkenne@gmail.com>
2015-01-22 10:32:17 +0100

    okokok

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,14 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 19:36:25 by averkenn          #+#    #+#             */
-/*   Updated: 2014/12/23 19:37:48 by averkenn         ###   ########.fr       */
+/*   Created: 2015/01/22 10:20:42 by averkenn          #+#    #+#             */
+/*   Updated: 2015/01/22 10:31:57 by averkenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int c, int fd)
 {
-	
+	if (n = -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
 }
