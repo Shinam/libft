9a2e4864988d3138769d5b439bea785a8c6016fd
averkenn <arnaud.verkenne@gmail.com>
2015-01-22 11:41:30 +0100

    lolgithublol

---
+++ ft_strsplit.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 11:26:23 by averkenn          #+#    #+#             */
+/*   Updated: 2015/01/22 11:40:49 by averkenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**str;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	str = malloc(sizeof(s) * *(ft_strlen(s) + 1));
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i])
+			{
+				str[k][j] = s[i];
+				j++;
+				i++;
+			}
+			str[k][j] = '\0';
+			k++;
+		}
+		else
+			i++;
+	}
+	return (str);
+}
