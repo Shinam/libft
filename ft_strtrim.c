b1996ce098626c775cbbf4cb5daae861395a128b
averkenn <arnaud.verkenne@gmail.com>
2015-02-07 22:17:59 +0100

    good

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 11:15:27 by averkenn          #+#    #+#             */
-/*   Updated: 2015/02/07 22:05:19 by averkenn         ###   ########.fr       */
+/*   Updated: 2015/02/07 22:15:22 by averkenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ char	*ft_strtrim(char const *s)
 	while (ft_isspace(s[len]) && len > 0)
 		len--;
 	if (len == 0)
-		return (NULL);
+		return ("");
 	len = len - start;
 	str = ft_strsub(s, start, len + 1);
 	if (!str)
-		return (NULL);
+		return ("");
 	return (str);
 }
