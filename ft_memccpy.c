7efb52d181ff54dbd86fc00e27b859b333de48e7
averkenn <arnaud.verkenne@gmail.com>
2015-02-15 04:12:14 +0100

    lol

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 03:47:24 by averkenn          #+#    #+#             */
-/*   Updated: 2015/02/15 03:44:26 by averkenn         ###   ########.fr       */
+/*   Updated: 2015/02/15 04:11:30 by averkenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		if (((unsigned char *)src)[i] == (unsigned char)c)
-			return(dst + i + 1);
+			return (dst + i + 1);
 		i++;
 	}
 	return (0);
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,35 +6,59 @@
 /*   By: averkenn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 11:26:23 by averkenn          #+#    #+#             */
-/*   Updated: 2015/02/09 01:43:14 by averkenn         ###   ########.fr       */
+/*   Updated: 2015/02/15 04:02:54 by averkenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+static int		ft_strdiv(char const *s, char c)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 0;
+	if (!s)
+		return (0);
+	while (s[i] == c && s[i])
+		i++;
+	while (s[i])
+	{
+		k++;
+		while (s[i] != c && s[i])
+			i++;
+		while (s[i] == c && s[i])
+			i++;
+	}
+	return (k);
+}
+
+char			**ft_strsplit(char const *s, char c)
 {
 	int		i;
 	int		j;
 	int		k;
-	char	**str;
+	char	**tab;
 
 	i = 0;
 	j = 0;
-	k = 0;
-	str = (char **)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!s)
-		return (NULL);
+	tab = (char **)malloc((ft_strdiv(s, c) + 1) * sizeof(char *));
 	while (s[i])
 	{
-		if (s[i] != c)
-		{
-			while (s[i] != c && s[i])
-				str[k][j++] = s[i++];
-			str[k++][j] = '\0';
-		}
-		else
+		while (s[i] == c && s[i])
 			i++;
+		if (!s[i])
+			break ;
+		k = 0;
+		while (s[i + k] != c && s[i + k])
+			k++;
+		tab[j] = ft_strnew(k + 1);
+		ft_strncpy(tab[j], &s[i], k);
+		tab[j][k] = 0;
+		i += ft_strlen(tab[j]);
+		j = (k) ? j + 1 : j;
 	}
-	return (str);
+	tab[j] = NULL;
+	return (tab);
 }
